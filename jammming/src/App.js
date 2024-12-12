17ffbd1876337a5efe65967fef68f1bd9023b216
Michael Skirka <skirkamp@hotmail.com>
2024-12-12 20:56:19 +0000

    updated app.js

--- jammming/src/App.js
+++ jammming/src/App.js
@@ -5,13 +5,21 @@ import { Playlist } from './Playlist';
 import './App.css';
 
 function App() {
+// Hardcoded array of track objects
+  const [tracks] = useState ([
+    { id: 1, name: 'Track 1', artist: 'Artist 1', album: 'Album 1' },
+    { id: 2, name: 'Track 2', artist: 'Artist 2', album: 'Album 2' },
+    { id: 3, name: 'Track 3', artist: 'Artist 3', album: 'Album 3' },
+    { id: 4, name: 'Track 4', artist: 'Artist 4', album: 'Album 4' },
+  ]);
+
   return (
     <div className="App">
       <h1>Jammming</h1>
       <SearchBar />
       <div className="App-content">
-        <SearchResults />
-        <Playlist />
+        <SearchResults tracks={tracks} />
+        <Playlist tracks={tracks} />
       </div>
     </div>
   );
