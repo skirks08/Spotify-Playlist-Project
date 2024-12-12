42e8c7032a4b2f8b2dbfc4a154f073a0227b712f
Michael Skirka <skirkamp@hotmail.com>
2024-12-12 21:16:43 +0000

    updated js for implementing playlists

--- jammming/src/App.js
+++ jammming/src/App.js
@@ -5,12 +5,15 @@ import { Playlist } from './Playlist';
 import './App.css';
 
 function App() {
-// Hardcoded array of track objects
-  const [tracks] = useState ([
-    { id: 1, name: 'Track 1', artist: 'Artist 1', album: 'Album 1' },
-    { id: 2, name: 'Track 2', artist: 'Artist 2', album: 'Album 2' },
-    { id: 3, name: 'Track 3', artist: 'Artist 3', album: 'Album 3' },
-    { id: 4, name: 'Track 4', artist: 'Artist 4', album: 'Album 4' },
+// Hardcoded playlist data for testing
+
+  const [playlist, setPlaylist] = useState ([
+    name: 'My Playlist',
+    tracks: [
+      { id: 1, name: 'Track 1', artist: 'Artist 1', album: 'Album 1' },
+      { id: 2, name: 'Track 2', artist: 'Artist 2', album: 'Album 2' },
+      { id: 3, name: 'Track 3', artist: 'Artist 3', album: 'Album 3' },
+    ]
   ]);
 
   return (
@@ -19,7 +22,7 @@ function App() {
       <SearchBar />
       <div className="App-content">
         <SearchResults tracks={tracks} />
-        <Playlist tracks={tracks} />
+        <Playlist tracks={tracks} playlist={playlist} setPlaylist={setPlaylist}/>
       </div>
     </div>
   );
--- jammming/src/Playlist.js
+++ jammming/src/Playlist.js
@@ -2,11 +2,22 @@ import React from 'react';
 import { Tracklist } from './Tracklist';
 import './Playlist.css';
 
-export function Playlist({ tracks }) {
+export function Playlist({ playlist, setPlaylist }) {
+
+  const [playlistName, setPlaylistName] = useState(playlist.name);
+
+  const handleNameChange = (e) => {
+    setPlaylistName(e.target.value);
+    setPlaylist({
+        ...playlist,
+        name: e.target.value,
+    });
+  };
+
   return (
     <div className="Playlist">
-      <input defaultValue="New Playlist" />
-      <Tracklist tracks={tracks} />
+      <input value={playlistName} onChange={handleNameChange} />
+      <Tracklist tracks={playlist.tracks} />
       <button className="SaveButton">Save To Spotify</button>
     </div>
   );
