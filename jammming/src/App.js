f7d8200faaa24cb552a92bb0609c3bd633c39a60
Michael Skirka <skirkamp@hotmail.com>
2024-12-13 15:20:29 +0000

    re added hardcoded array of track objects into app.js

--- jammming/src/App.js
+++ jammming/src/App.js
@@ -5,6 +5,14 @@ import { Playlist } from './Playlist';
 import './App.css';
 
 function App() {
+  // Hardcoded array of track objects
+  const [tracks] = useState([
+    { id: 1, name: 'Track 1', artist: 'Artist 1', album: 'Album 1' },
+    { id: 2, name: 'Track 2', artist: 'Artist 2', album: 'Album 2' },
+    { id: 3, name: 'Track 3', artist: 'Artist 3', album: 'Album 3' },
+    { id: 4, name: 'Track 4', artist: 'Artist 4', album: 'Album 4' },
+  ]);
+  
   // Hardcoded playlist data for testing
   const [playlist, setPlaylist] = useState({
     name: 'My Playlist',
