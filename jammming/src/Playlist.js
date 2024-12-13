1885eb64f7f31e5cc30b686f813e1cd3da8dd9a2
Michael Skirka <skirkamp@hotmail.com>
2024-12-13 17:28:17 +0000

    correcting errors on playlist component

--- jammming/src/Playlist.js
+++ jammming/src/Playlist.js
@@ -21,4 +21,24 @@ export function Playlist({ playlist, updatePlaylistName, savePlaylist }) {
       </button>
     </div>
   );
-}
+};
+
+export function Tracklist ({ tracks, action }) {
+  return (
+    <div className="Tracklist">
+      {tracks.map((track) => (
+        <div className="Track" key={track.id}>
+          <div className="Track-information">
+            <h3>{track.name}</h3>
+            <p>{track.artist} | {track.album}</p>
+          </div>
+          {action && (
+            <button className="Track-action" onClick={() => action(track)}>
+              {action.label}
+            </button>
+          )}
+        </div>
+      ))}
+    </div>
+  );
+};
