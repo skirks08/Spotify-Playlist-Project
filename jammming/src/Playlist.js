d5d992db74b7e8683d51f35d583da73e8f03f953
Michael Skirka <skirkamp@hotmail.com>
2024-12-13 15:35:05 +0000

    updated save playlist in playlist.js

--- jammming/src/Playlist.js
+++ jammming/src/Playlist.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { Tracklist } from './Tracklist';
 import './Playlist.css';
 
-export function Playlist({ playlist, updatePlaylistName }) {
+export function Playlist({ playlist, updatePlaylistName, savePlaylist }) {
 
   const handleNameChange = (event) => {
     updatePlaylistName(event.target.value);
@@ -10,14 +10,15 @@ export function Playlist({ playlist, updatePlaylistName }) {
 
   return (
     <div className="Playlist">
-      {/* Editable input for the playlist name */}
       <input
         className="Playlist-name"
         value={playlist.name}
         onChange={handleNameChange}
       />
       <Tracklist tracks={playlist.tracks} />
-      <button className="SaveButton">Save To Spotify</button>
+      <button className="SaveButton" onClick={savePlaylist}>
+        Save To Spotify
+      </button>
     </div>
   );
 }
