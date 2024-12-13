5a3df505a92f357aa0768cf27a2832d6a4367e3d
Michael Skirka <skirkamp@hotmail.com>
2024-12-13 17:17:11 +0000

    imported useState from react in app and search bar component

--- jammming/src/App.js
+++ jammming/src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import { useState, React } from 'react';
 import { SearchBar } from './SearchBar';
 import { SearchResults } from './SearchResults';
 import { Playlist } from './Playlist';
