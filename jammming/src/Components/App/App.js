5c5cb203594721d7827bd7253773dc1307a5050b
Michael Skirka <skirkamp@hotmail.com>
2024-12-16 14:31:23 +0000

    updated import for app js in index js

--- jammming/src/Components/App/App.js
+++ jammming/src/Components/App/App.js
@@ -1,5 +1,5 @@
 import React, { useState, useCallback } from "react";
-import "./App.css";
+import "./App.css"; 
 
 import Playlist from "../Playlist/Playlist";
 import SearchBar from "../SearchBar/SearchBar";
